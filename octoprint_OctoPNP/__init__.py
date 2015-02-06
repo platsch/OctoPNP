099789d21738229d5a618178c485fc5e7799bc27
Florens Wasserfall <wasserfall@kalanka.de>
2015-02-06 19:36:39 +0100

    basic testing for hooks and events

--- octoprint_OctoPNP/__init__.py
+++ octoprint_OctoPNP/__init__.py
@@ -3,15 +3,26 @@ from __future__ import absolute_import
 
 
 import octoprint.plugin
+import time
+import re
+from .SmdParts import SmdParts
+
 
 class OctoPNP(octoprint.plugin.StartupPlugin,
 			  octoprint.plugin.TemplatePlugin,
 			  octoprint.plugin.EventHandlerPlugin,
 			  octoprint.plugin.SettingsPlugin):
 
+	smdparts = SmdParts()
+
+	def __init__(self):
+		pass
+
+
 	def on_after_startup(self):
-		self._logger.info("Hello World! setting: %s" % self._settings.get(["tray_x"]))
-		print self._settings.get(["tray_x"])
+		#self._logger.info("Hello World! setting: %s" % self._settings.get(["tray_x"]))
+		#print self._settings.get(["tray_x"])
+		pass
 
 	def get_settings_defaults(self):
 		return dict(tray_x="teststring")
@@ -21,10 +32,29 @@ class OctoPNP(octoprint.plugin.StartupPlugin,
 
 
 	def on_event(self, event, payload):
-		self._logger.info("Event: " + event)
-		if event == "PrintPaused":
-			self._logger.info("send command: G1 X0 F5000")
-			self._printer.command("G1 X0 F5000")
+		if event == "FileSelected":
+			self._logger.info("file: " + payload.get("file"))
+			xml = "";
+			f = open(payload.get("file"), 'r')
+			for line in f:
+				expression = re.search("<.*>", line)
+				if expression:
+					xml += expression.group() + "\n"
+					print expression.group()
+			if xml:
+				#check for root node existance
+				if not re.search("<object.*>", xml.splitlines()[0]):
+					xml = "<object name=\"defaultpart\">\n" + xml + "\n</object>"
+
+				#parse xml data
+				self.smdparts.load(xml)
+
+				print self.smdparts.getPartPosition(3)
+			else:
+				self.smdparts.unload()
+
+		#	self._printer.command("G1 X0 F5000")
+
 
 	def get_template_configs(self):
 		return [
@@ -33,8 +63,36 @@ class OctoPNP(octoprint.plugin.StartupPlugin,
 			#dict(type="tab", data_bind="stateString: teststring")
 		]
 
+	def placePart(self, partnr):
+		print "placing part nr " + str(partnr) + " which is at position " + str(self.smdparts.getPartPosition(3))
+
+
+
+
+def hook_gcode(comm_obj, cmd):
+	command = re.search("M361\s*P\d*", cmd)
+	if command:
+		print "hook, command: " + command.group()
+		command = re.search("P\d*", command.group()).group() #strip the M361
+		opnp = OctoPNP()
+		opnp.placePart(int(command[1:]))
+
+	#t=OctoPNP()
+	#t.testprint()
+	"""if cmd == "G92 E0":
+		print "sleep"
+#		self._printer.command("M1234 P0")
+		#opnp.testprint()
+		time.sleep(10)
+		comm_obj._doSend("M1234 P0")
+		print "send M1234"
+		time.sleep(10)
+		return "M4321"
+		"""
+
 # If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
 # ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
 # can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
 __plugin_name__ = "OctoPNP"
+__plugin_hooks__ = {'octoprint.comm.protocol.gcode': hook_gcode}
 __plugin_implementations__ = [OctoPNP()]
