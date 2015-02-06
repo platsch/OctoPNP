a5414c9463effd9eae025a1131af4a6286239533
Florens Wasserfall <wasserfall@kalanka.de>
2015-02-06 12:38:23 +0100

    XPath for finding element properties

--- octoprint_OctoPNP/SmdParts.py
+++ octoprint_OctoPNP/SmdParts.py
@@ -12,11 +12,23 @@ class SmdParts():
 	def load(self, xmlstring):
 		self._et = ET.fromstring(xmlstring)
 
+		#print content for debug
+		print "root tag: " + self._et.tag
+		print "root attribute: "
+		print self._et.attrib
+
+		for child in self._et:
+			print "child t, a: "
+			print child.tag, child.attrib
+			print "\n"
+
 	def getPartCount(self):
 		pass
 
+	#return the nr of the box this part is supposed to be in
 	def getPartPosition(self, partnr):
-		pass
+		return int(self._et.find("./part[@id='" + str(partnr) + "']/position").get("box"))
 
-	def _parse(self, file):
+
+	def getPartDestination(self, partnr):
 		pass
