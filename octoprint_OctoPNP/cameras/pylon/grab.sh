3d9dafa9a0ef853e20f35296b07960a457b2a0a0
Florens Wasserfall <wasserfall@kalanka.de>
2015-02-18 15:21:32 +0100

    fixed exec path in grab wrapper

--- octoprint_OctoPNP/cameras/pylon/grab.sh
+++ octoprint_OctoPNP/cameras/pylon/grab.sh
@@ -6,5 +6,9 @@ source /opt/pylon4/bin/pylon-setup-env.sh /opt/pylon4
 #disable usb cameras to avoid conflict between on board built-in Webcam or USB2 Web Cam and Pylon4Linux V4.
 export PYLON_USE_TL=BaslerGigE
 
+ROOT=$(readlink -f $(dirname $0))
+
+pwd
 #start the image grabber
-./grab
+$ROOT/grab
+
