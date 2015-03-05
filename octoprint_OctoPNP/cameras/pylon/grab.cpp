d4385069c054a2957e1994069d1f4a44a3d872e8
Florens Wasserfall <wasserfall@kalanka.de>
2015-03-05 08:37:28 +0100

    license typo

--- octoprint_OctoPNP/cameras/pylon/grab.cpp
+++ octoprint_OctoPNP/cameras/pylon/grab.cpp
@@ -12,7 +12,7 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Repetier-Firmware.  If not, see <http://www.gnu.org/licenses/>.
+    along with OctoPNP.  If not, see <http://www.gnu.org/licenses/>.
 
     Main author: Florens Wasserfall <wasserfall@informatik.uni-hamburg.de>
 */
