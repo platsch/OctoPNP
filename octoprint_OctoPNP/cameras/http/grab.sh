6c69c8d9b723f2317d59ae18ea2e165b53449a67
Demo User <debian@beaglebone.localdomain>
2015-10-09 11:01:45 +0000

    fixed wget output option

--- octoprint_OctoPNP/cameras/http/grab.sh
+++ octoprint_OctoPNP/cameras/http/grab.sh
@@ -2,8 +2,8 @@
 
 ROOT=$(readlink -f $(dirname $0))
 
-wget http://192.168.100.165/liveimg.cgi $ROOT/head.jpg
-wget http://192.168.100.167/liveimg.cgi $ROOT/bed.jpg
+wget http://192.168.100.165/liveimg.cgi -O $ROOT/head.jpg
+wget http://192.168.100.167/liveimg.cgi -O $ROOT/bed.jpg
 
-convert $ROOT/head.jpg -rotate 90 $ROOT/../head.png
-convert $ROOT/bed.jpg $ROOT/../bed.png
+convert $ROOT/head.jpg -rotate 90 $ROOT/../head.jpg
+convert $ROOT/bed.jpg $ROOT/../bed.jpg
