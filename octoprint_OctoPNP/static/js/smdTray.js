5eca50ee0f20c7a2dd79abc1c0d87db9cc92cd76
Florens Wasserfall <wasserfall@kalanka.de>
2015-11-24 14:00:37 +0100

    refactoring of smd handling in js and interactive execution of pick n place

--- octoprint_OctoPNP/static/js/smdTray.js
+++ octoprint_OctoPNP/static/js/smdTray.js
@@ -1,20 +1,53 @@
 function smdTray(cols, rows, boxSize, canvas) {
 	var self = this;
 
-	self.cols = cols;
-    self.rows = rows;
-    self.trayBoxSize = boxSize;
-    self.trayCanvas = canvas;
-    self.parts = {};
+	var _cols = cols;
+    var _rows= rows;
+    var _trayBoxSize = boxSize;
+    var _trayCanvas = canvas;
+    var _parts = {};
+
+    self.erase = function() {
+        _parts = {};
+        _drawTray();
+    }
+
+    self.addPart = function(part) {
+        // sanitiy checks!?
+        // add part to dict
+        _parts[part.id] = part;
+
+        _parts[part.id].row = parseInt(((part.partPosition-1) / _cols)) + 1;
+        _parts[part.id].col = (part.partPosition-1) % _cols+1;
+
+        // and draw to canvas
+        _drawPart(part.id, "black");
+    }
+
+    self.selectPart = function(x, y) {
+        var canvasBoxSize = _getCanvasBoxSize();
+        col = Math.floor(x/(canvasBoxSize+1)) + 1;
+        row = Math.floor(((_rows*canvasBoxSize)-y)/(canvasBoxSize-1)) + 1;
+
+        for (var id in _parts) {
+            _drawPart(id, "black");
+        }
+
+        var partId = _getPartId(col, row);
+        if(partId) {
+            _drawPart(partId, "red");
+        }
+        return partId;
+    }
 
 
-	self.drawTray = function() {
-		if (self.trayCanvas && self.trayCanvas.getContext) {
-            var ctx = self.trayCanvas.getContext("2d");
+	function _drawTray () {
+		if (_trayCanvas && _trayCanvas.getContext) {
+            var ctx = _trayCanvas.getContext("2d");
             if (ctx) {
                 var size_x = ctx.canvas.width;
                 var size_y = ctx.canvas.height;
-                var canvasBoxSize = self.getCanvasBoxSize();
+                var canvasBoxSize = _getCanvasBoxSize();
 
                 //initialize white tray
                 ctx.strokeStyle = "black";
@@ -23,9 +56,9 @@ function smdTray(cols, rows, boxSize, canvas) {
                 ctx.fillRect(0,0,size_x,size_y);
                 ctx.strokeRect (0,0,size_x,size_y);
 
-				for(var x=0; x<self.cols; x++) {
-                    for(var y=0; y<self.rows; y++) {
-                        self.drawTrayBox(x, y, canvasBoxSize);
+				for(var x=0; x<_cols; x++) {
+                    for(var y=0; y<_rows; y++) {
+                        _drawTrayBox(x+1, y+1, canvasBoxSize);
                     }
                 }
             }
@@ -34,21 +67,19 @@ function smdTray(cols, rows, boxSize, canvas) {
 	
 	
 	//draw a part into a tray box
-    self.drawPart = function(part) {
-        console.log(part.name);
-        var row = parseInt(((part.partPosition-1) / self.cols)) + 1;
-		var col = (part.partPosition-1) % self.cols+1;
+    function _drawPart(partID, color) {
+        part = _parts[partID];
 
 		//clear old box
-		var canvasBoxSize = self.getCanvasBoxSize();
-		self.drawTrayBox(col-1, row-1, canvasBoxSize);
+		var canvasBoxSize = _getCanvasBoxSize();
+		_drawTrayBox(part.col, part.row, canvasBoxSize);
 
-		if (self.trayCanvas && self.trayCanvas.getContext) {
-            var ctx = self.trayCanvas.getContext("2d");
-            var scale = canvasBoxSize/self.trayBoxSize;
+		if (_trayCanvas && _trayCanvas.getContext) {
+            var ctx = _trayCanvas.getContext("2d");
+            var scale = canvasBoxSize/_trayBoxSize;
             if (ctx) {
-                var col_offset = col*canvasBoxSize-canvasBoxSize+4;
-                var row_offset = self.rows*canvasBoxSize-row*canvasBoxSize+4;
+                var col_offset = part.col*canvasBoxSize-canvasBoxSize+4;
+                var row_offset = _rows*canvasBoxSize-part.row*canvasBoxSize+4;
 
                 //print part names
 				ctx.font = "10px Verdana";
@@ -61,9 +92,9 @@ function smdTray(cols, rows, boxSize, canvas) {
 					var points = part.shape;
 
 					ctx.beginPath();
-					ctx.strokeStyle = "#000000";
+					ctx.strokeStyle = color;
 					ctx.lineWidth = 1;
-					ctx.fillStyle = "#000000";
+					ctx.fillStyle = color;
 					if(points.length > 0) {
 						ctx.moveTo(points[0][0]*scale+col_offset+canvasBoxSize/2, points[0][1]*scale+row_offset+canvasBoxSize/2);
 						for(var i=0; i < points.length; i++) {
@@ -90,30 +121,45 @@ function smdTray(cols, rows, boxSize, canvas) {
         }
     }
 
-    //draw a single tray box
-    self.drawTrayBox = function(col, row, size) {
-        if (self.trayCanvas && self.trayCanvas.getContext) {
-            var ctx = self.trayCanvas.getContext("2d");
+    // draw a single tray box
+    function _drawTrayBox(col, row, size) {
+        col -=1;
+        row -=1;
+        if (_trayCanvas && _trayCanvas.getContext) {
+            var ctx = _trayCanvas.getContext("2d");
             if (ctx) {
                 ctx.lineWidth = 4;
                 ctx.strokeStyle = "green";
                 ctx.fillStyle = "white";
-                ctx.strokeRect (col*size+ctx.lineWidth/2,(self.rows-1)*size-row*size+ctx.lineWidth/2,size-ctx.lineWidth/2,size-ctx.lineWidth/2);
-                ctx.fillRect (col*size+ctx.lineWidth,(self.rows-1)*size-row*size+ctx.lineWidth,size-ctx.lineWidth,size-ctx.lineWidth);
+                ctx.strokeRect (col*size+ctx.lineWidth/2,(_rows-1)*size-row*size+ctx.lineWidth/2,size-ctx.lineWidth/2,size-ctx.lineWidth/2);
+                ctx.fillRect (col*size+ctx.lineWidth,(_rows-1)*size-row*size+ctx.lineWidth,size-ctx.lineWidth,size-ctx.lineWidth);
             }
         }
     }
 
-    self.getCanvasBoxSize = function() {
+    // returns the box size to use the available canvas-space in an optimal way
+    function _getCanvasBoxSize() {
         var boxSize = 0;
-        if (self.trayCanvas && self.trayCanvas.getContext) {
-            var ctx = self.trayCanvas.getContext("2d");
+        if (_trayCanvas && _trayCanvas.getContext) {
+            var ctx = _trayCanvas.getContext("2d");
             if (ctx) {
                 var size_x = ctx.canvas.width;
                 var size_y = ctx.canvas.height;
-                boxSize = Math.min((size_x-4)/self.cols, (size_y-4)/self.rows);
+                boxSize = Math.min((size_x-4)/_cols, (size_y-4)/_rows);
             }
         }
-        return boxSize;
+        return Math.floor(boxSize);
+    }
+
+    // select partId from col/row
+    function _getPartId(col, row) {
+        var result = false;
+        for (var id in _parts) {
+            if((_parts[id].col == col) && (_parts[id].row == row)) {
+                result = id;
+                break;
+            }
+        }
+        return result;
     }
 }
