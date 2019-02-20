a0f1e90f619bc10e08184868af5155c28b208c03
Florens Wasserfall <wasserfall@kalanka.de>
2019-02-20 09:52:07 +0100

    Script for fetching images from local camera

--- octoprint_OctoPNP/cameras/grab.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash -e
-
-ROOT=$(readlink -f $(dirname $0))
-$ROOT/pylon/grab.sh
-$ROOT/http/grab.sh
-
---
+++ octoprint_OctoPNP/cameras/local/grab.sh
@@ -0,0 +1,8 @@
+#! /bin/bash -e
+
+ROOT=$(readlink -f $(dirname $0))
+
+streamer -s [640x480] -o $ROOT/head.jpeg
+
+convert $ROOT/head.jpeg  $ROOT/../head.jpeg
+convert $ROOT/head.jpeg $ROOT/../bed.jpeg
