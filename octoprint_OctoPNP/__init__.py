cc2881fcacb60d64ceb03590f864e25dc83cc34d
Florens Wasserfall <wasserfall@kalanka.de>
2020-06-10 12:48:39 +0000

    fix broken int conversion and syntax

--- octoprint_OctoPNP/__init__.py
+++ octoprint_OctoPNP/__init__.py
@@ -515,7 +515,7 @@ class OctoPNP(octoprint.plugin.StartupPlugin,
     # get the position of the box (center of the box) containing part x relative to the [0,0] corner of the tray
     def _getTrayPosFromPartNr(self, partnr):
         partPos = self.smdparts.getPartPosition(partnr)
-        row = (partPos-1)/int(self._settings.get(["tray", "columns"]))+1
+        row = int((partPos-1)/int(self._settings.get(["tray", "columns"]))+1)
         col = ((partPos-1)%int(self._settings.get(["tray", "columns"])))+1
         self._logger.info("Selected object: %d. Position: box %d, row %d, col %d", partnr, partPos, row, col)
 
@@ -627,7 +627,7 @@ class OctoPNP(octoprint.plugin.StartupPlugin,
             # open image and convert to base64
             f = open(parameter,"rb")
             data = dict(
-                src = "data:image/" + os.path.splitext(parameter)[1] + ";base64,"+str(base64.b64encode(bytes(f.read())), "utf-8"))
+                src = "data:image/" + os.path.splitext(parameter)[1] + ";base64,"+str(base64.b64encode(bytes(f.read())), "utf-8")
             )
 
         message = dict(
