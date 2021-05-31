4b7dc658a9e7dd3695b1b05f01c07a0cc6ba5d13
Jan-Tarek Butt <tarek@ring0.de>
2021-05-31 17:28:50 +0200

    ImageProcessingCaller.py: reformat file
    Signed-off-by: Jan-Tarek Butt <tarek@ring0.de>

--- octoprint_OctoPNP/ImageProcessingCaller.py
+++ octoprint_OctoPNP/ImageProcessingCaller.py
@@ -17,12 +17,12 @@ im = ImageProcessing.ImageProcessing(15.0, 120, 120)
 start_time = time.time()
 
 im._interactive = True
-#im.locatePartInBox("../utils/testimages/head_atmega_SO8.png", False)
-#im.locatePartInBox("../utils/testimages/head_atmega_SO8_2.png", False)
-#print im.getPartOrientation("../utils/testimages/bed_atmega_SO8_rotated.png", 30)
+# im.locatePartInBox("../utils/testimages/head_atmega_SO8.png", False)
+# im.locatePartInBox("../utils/testimages/head_atmega_SO8_2.png", False)
+# print im.getPartOrientation("../utils/testimages/bed_atmega_SO8_rotated.png", 30)
 im.getPartOrientation("../utils/testimages/orientation_bed_atmega_SO8_green.png", 55.65)
-#im.getPartPosition("../utils/testimages/orientation_bed_atmega_SO8_green.png", 55.65)
-#im.getPartOrientation("../utils/testimages/bed_resistor_1206.png", 55.65)
+# im.getPartPosition("../utils/testimages/orientation_bed_atmega_SO8_green.png", 55.65)
+# im.getPartOrientation("../utils/testimages/bed_resistor_1206.png", 55.65)
 
 end_time = time.time()
 print("--- %s seconds ---" % (time.time() - start_time))
