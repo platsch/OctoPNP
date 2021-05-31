521b783280495d3ab347f8af58b8c90f9ab4090e
Jan-Tarek Butt <tarek@ring0.de>
2021-05-31 16:49:01 +0200

    pylon/grab_bed.sh: prevent globbing and word splitting
    Signed-off-by: Jan-Tarek Butt <tarek@ring0.de>

--- octoprint_OctoPNP/cameras/pylon/grab_bed.sh
+++ octoprint_OctoPNP/cameras/pylon/grab_bed.sh
@@ -6,12 +6,12 @@ source /opt/pylon5/bin/pylon-setup-env.sh /opt/pylon5
 #disable usb cameras to avoid conflict between on board built-in Webcam or USB2 Web Cam and Pylon4Linux V4.
 export PYLON_USE_TL=BaslerGigE
 
-ROOT=$(readlink -f $(dirname $0))
+ROOT=$(readlink -f "$(dirname "$0")")
 
 pwd
 #start the image grabber
-$ROOT/grab $ROOT bed
+"$ROOT"/grab "$ROOT" bed
 
 #convert $ROOT/head.tiff -rotate 270 $ROOT/../head.png
 #rm $ROOT/head.tiff
-convert $ROOT/bed.tiff -rotate 270 $ROOT/../bed.png
+convert "$ROOT"/bed.tiff -rotate 270 "$ROOT"/../bed.png
