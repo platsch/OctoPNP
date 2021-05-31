f46031dff58b90eb3822e4ba09a4fddc8a841ab3
Jan-Tarek Butt <tarek@ring0.de>
2021-05-31 17:29:14 +0200

    SmdParts.py: reformat file
    Signed-off-by: Jan-Tarek Butt <tarek@ring0.de>

--- octoprint_OctoPNP/SmdParts.py
+++ octoprint_OctoPNP/SmdParts.py
@@ -2,13 +2,13 @@
 from __future__ import absolute_import
 
 __author__ = "Florens Wasserfall <wasserfall@kalanka.de>"
-__license__ = 'GNU Affero General Public License http://www.gnu.org/licenses/agpl.html'
+__license__ = "GNU Affero General Public License http://www.gnu.org/licenses/agpl.html"
 
 
 import xml.etree.ElementTree as ET
 
-class SmdParts():
 
+class SmdParts:
     def __init__(self):
         self._et = None
         self._positions = {}
@@ -39,7 +39,6 @@ class SmdParts():
             count += 1
         return count
 
-
     # returns a list of all available parts
     def getPartIds(self):
         result = []
@@ -67,28 +66,46 @@ class SmdParts():
         return self._et.find("./part[@id='" + str(partnr) + "']").get("name")
 
     def getPartHeight(self, partnr):
-        return float(self._et.find("./part[@id='" + str(partnr) + "']/size").get("height"))
+        return float(
+            self._et.find("./part[@id='" + str(partnr) + "']/size").get("height")
+        )
 
     def getPartShape(self, partnr):
         result = []
-        if(self._et.find("./part[@id='" + str(partnr) + "']/shape") is not None):
+        if self._et.find("./part[@id='" + str(partnr) + "']/shape") is not None:
             for elem in self._et.find("./part[@id='" + str(partnr) + "']/shape"):
                 result.append([float(elem.get("x")), float(elem.get("y"))])
         return result
 
     def getPartPads(self, partnr):
         result = []
-        if( self._et.find("./part[@id='" + str(partnr) + "']/pads") is not None):
+        if self._et.find("./part[@id='" + str(partnr) + "']/pads") is not None:
             for elem in self._et.find("./part[@id='" + str(partnr) + "']/pads"):
-                result.append([float(elem.get("x1")), float(elem.get("y1")), float(elem.get("x2")), float(elem.get("y2"))])
+                result.append(
+                    [
+                        float(elem.get("x1")),
+                        float(elem.get("y1")),
+                        float(elem.get("x2")),
+                        float(elem.get("y2")),
+                    ]
+                )
         return result
 
-
     def getPartDestination(self, partnr):
-        x = float(self._et.find("./part[@id='" + str(partnr) + "']/destination").get("x"))
-        y = float(self._et.find("./part[@id='" + str(partnr) + "']/destination").get("y"))
-        z = float(self._et.find("./part[@id='" + str(partnr) + "']/destination").get("z"))
-        orientation = float(self._et.find("./part[@id='" + str(partnr) + "']/destination").get("orientation"))
+        x = float(
+            self._et.find("./part[@id='" + str(partnr) + "']/destination").get("x")
+        )
+        y = float(
+            self._et.find("./part[@id='" + str(partnr) + "']/destination").get("y")
+        )
+        z = float(
+            self._et.find("./part[@id='" + str(partnr) + "']/destination").get("z")
+        )
+        orientation = float(
+            self._et.find("./part[@id='" + str(partnr) + "']/destination").get(
+                "orientation"
+            )
+        )
         return [x, y, z, orientation]
 
     def _sanitize(self):
@@ -128,34 +145,45 @@ class SmdParts():
                     result, msg = self._sanitizeTag(part, "size", ["height"], float)
                 # shape
                 if result:
-                    if(part.find("shape") is not None):
+                    if part.find("shape") is not None:
                         for elem in part.find("shape"):
-                            result, msg = self._sanitizeAttribute(part, elem, ["x", "y"], float)
+                            result, msg = self._sanitizeAttribute(
+                                part, elem, ["x", "y"], float
+                            )
                 # pads
                 if result:
-                    if(part.find("pads") is not None):
+                    if part.find("pads") is not None:
                         for elem in part.find("pads"):
-                            result, msg = self._sanitizeAttribute(part, elem, ["x1", "y1", "x2", "y2"], float)
+                            result, msg = self._sanitizeAttribute(
+                                part, elem, ["x1", "y1", "x2", "y2"], float
+                            )
                 # destination
                 if result:
-                    result, msg = self._sanitizeTag(part, "destination", ["x", "y", "z", "orientation"], float)
+                    result, msg = self._sanitizeTag(
+                        part, "destination", ["x", "y", "z", "orientation"], float
+                    )
 
         return result, msg
-
 
     def _sanitizeTag(self, part, tag, attributes, validate):
         result = True
         msg = ""
 
         elem = part.find(tag)
-        if(elem is not None):
+        if elem is not None:
             result, msg = self._sanitizeAttribute(part, elem, attributes, validate)
         else:
             result = False
-            msg = "No tag " + tag + " in part " + part.get("id") + " - " + part.get("name")
+            msg = (
+                "No tag "
+                + tag
+                + " in part "
+                + part.get("id")
+                + " - "
+                + part.get("name")
+            )
 
         return result, msg
-
 
     def _sanitizeAttribute(self, part, elem, attributes, validate):
         result = True
@@ -167,6 +195,13 @@ class SmdParts():
                     validate(elem.get(attribute))
                 except:
                     result = False
-                    msg = "Invalid or no attribute " + attribute + " in part " + part.get("id") + " - " + part.get("name")
+                    msg = (
+                        "Invalid or no attribute "
+                        + attribute
+                        + " in part "
+                        + part.get("id")
+                        + " - "
+                        + part.get("name")
+                    )
 
         return result, msg
